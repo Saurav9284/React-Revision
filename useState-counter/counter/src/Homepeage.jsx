b39cf22b930e0165400b4030c2d5e736c609fae1
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-02-06 16:41:54 +0530

    Update Homepeage.jsx

--- useState-counter/counter/src/Homepeage.jsx
+++ useState-counter/counter/src/Homepeage.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+
 import Counter from './Components/Counter'
 
+
 const Homepeage = () => {
+  
   return (
+    
     <div>
+      
       <h1>Counter App</h1>
+      
       <Counter/>
+      
     </div>
+    
   )
 }
 
