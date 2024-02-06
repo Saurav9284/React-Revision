184c260db7dffc6cbb9af925ed5bfffccd970ef8
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-02-06 16:41:33 +0530

    Update Counter.jsx

--- useState-counter/counter/src/Components/Counter.jsx
+++ useState-counter/counter/src/Components/Counter.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import { useState } from 'react'
 
 const Counter = () => {
@@ -6,11 +7,17 @@ const Counter = () => {
     const [count,setCount] = useState(0);
 
   return (
+      
     <div>
+        
       <button onClick={()=>{setCount(count+1)}} disabled={count===30}>+</button>
+        
       {count}
+        
       <button onClick={()=>{setCount(count-1)}} disabled={count===0}>-</button>
+        
     </div>
+      
   )
 }
 
