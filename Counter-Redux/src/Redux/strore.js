8fcef43870d512baa5fda3f1104e68d6a7116f40
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-04-26 09:51:25 +0530

    Update strore.js

--- Counter-Redux/src/Redux/strore.js
+++ Counter-Redux/src/Redux/strore.js
@@ -1,6 +1,6 @@
-import { createStore } from 'redux';
+import { legacy_createStore } from 'redux';
 import counterReducer from './reducers/counterReducer';
 
-const store = createStore(counterReducer);
+const store = legacy_createStore(counterReducer);
 
 export default store;
